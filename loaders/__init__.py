f2e57fd692ca4979a25f3109b76a06c8fe3f9809
codsalah <salahhamada63@gmail.com>
2026-03-31 00:08:58 +0200

    Update: Clean imports and remove priority loader from package

---
+++ loaders/__init__.py
@@ -0,0 +1,14 @@
+from loaders.base_scd2_loader import BaseSCD2Loader, LoadResult
+from loaders.dim_customer_loader import DimCustomerLoader
+from loaders.dim_driver_loader import DimDriverLoader
+from loaders.dim_restaurant_loader import DimRestaurantLoader
+from loaders.dim_agent_loader import DimAgentLoader
+
+__all__ = [
+    "BaseSCD2Loader",
+    "LoadResult",
+    "DimCustomerLoader",
+    "DimDriverLoader",
+    "DimRestaurantLoader",
+    "DimAgentLoader",
+]
