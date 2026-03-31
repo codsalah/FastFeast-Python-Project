74c6160a4edfb0dbc701e3eaaf5572a126f71acd
Roaa Talat <roaatalat211@gmail.com>
2026-03-31 08:18:22 +0200

    feat: update __init__ exports for StaticDimLoader

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -1,8 +1,13 @@
+"""
+loaders/__init__.py
+"""
+
 from loaders.base_scd2_loader import BaseSCD2Loader, LoadResult
 from loaders.dim_customer_loader import DimCustomerLoader
 from loaders.dim_driver_loader import DimDriverLoader
 from loaders.dim_restaurant_loader import DimRestaurantLoader
 from loaders.dim_agent_loader import DimAgentLoader
+from loaders.dim_static_loader import StaticDimLoader, load_static_dimensions
 
 __all__ = [
     "BaseSCD2Loader",
@@ -11,4 +16,6 @@ __all__ = [
     "DimDriverLoader",
     "DimRestaurantLoader",
     "DimAgentLoader",
+    "StaticDimLoader",
+    "load_static_dimensions",
 ]
